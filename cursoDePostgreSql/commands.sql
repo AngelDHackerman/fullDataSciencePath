738cb49066657409baa4c525cc6518a32394231c
angelDariaux <angeldariaux@gmail.com>
2022-10-29 22:10:41 -0600

    agregando los commando basicos de postgres

---
+++ cursoDePostgreSql/commands.sql
@@ -0,0 +1,16 @@
+-- comando para iniciar postgres:
+`sudo -u postgres psql`
+
+`\l` -- Lista todas las bases de datos 
+
+`\h` -- lista todos los comandos disponibles 
+
+`\h command_name` -- Muestra como funciona el comando mencionado 
+
+`\c dataBase_name` -- cambia a la base de datos deseada. 
+
+`SELECT version();` -- Nos da la version utilizada en postgres. 
+
+`\g` -- ejecuta el comando ejecutado previamente. 
+
+`\timing` -- muestra el tiempo que tomo ejecutar los comandos 
