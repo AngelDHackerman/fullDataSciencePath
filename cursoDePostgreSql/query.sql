6c7eb8cdf6028143fc994c50ede9fc57fb7a1026
angelDariaux <angeldariaux@gmail.com>
2022-11-05 20:13:31 -0600

    usando PL o bloques en sql

--- cursoDePostgreSql/query.sql
+++ cursoDePostgreSql/query.sql
@@ -89,7 +89,7 @@ SELECT *
 
 
 
--- Vistas materializadas 
+-- ? Vistas materializadas 
 
 	-- Definicion de la vista materializada: 
 
@@ -103,3 +103,38 @@ SELECT viaje.id,
 
 
 REFRESH MATERIALIZED VIEW despues_noche_mview;
+
+
+
+-- ? PL o bloques de codigo
+
+DO $$  -- se abre con 'DO' y con $$ tambien se cierra con $$
+BEGIN
+	RAISE NOTICE 'ALGO ESTA PASANDO';
+END
+$$
+
+
+DO $$
+DECLARE 
+rec record; -- rec, es la variable; record, es el tipo de variable
+BEGIN
+	FOR rec IN SELECT * FROM pasajero LOOP -- por cada columna de pasajero se crea un loop y se asigna a 'rec'
+	RAISE NOTICE 'Un pasajero se llama %', rec.nombre; -- el %, es un comodin para podre agregar las propiedades de la variable rec.nombre
+	END LOOP; -- con eso le decimos que se termina el loop 
+END
+$$
+
+
+DO $$
+DECLARE 
+rec record;
+contador integer := 0; -- contador: es la variable; integer: el tipo de variable; := asi es como se asignan los valores
+BEGIN
+	FOR rec IN SELECT * FROM pasajero LOOP 
+	RAISE NOTICE 'Un pasajero se llama %', rec.nombre;
+	contador := contador + 1;  -- esto hace crecer el contador
+	END LOOP;
+	RAISE NOTICE 'Conteo es %', contador; -- el valor de 'contador' se introducira en el '%'
+END
+$$
