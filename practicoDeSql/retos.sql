970ff5230f198cd2c6bf0baf3b351d6a08cd4165
angelDariaux <angeldariaux@gmail.com>
2022-11-13 20:29:22 -0600

    comenzando los retos

---
+++ practicoDeSql/retos.sql
@@ -0,0 +1,24 @@
+
+SELECT * from platzi.alumnos
+FETCH FIRST 1 ROWS ONLY;  -- esto trae el primer resultado, es el estandar de SQL server
+
+SELECT * from platzi.alumnos
+limit 1; -- mismo de arriba pero este es mySql
+
+
+
+-- ? Mostrando los primeros 5 resultados: 
+
+SELECT * FROM platzi.alumnos 
+FETCH FIRST 5 ROWS ONLY; 
+
+
+SELECT * FROM platzi.alumnos 
+LIMIT 5; 
+
+
+SELECT * FROM (
+	SELECT ROW_NUMBER() OVER() AS row_id, * 
+	FROM platzi.alumnos
+) AS alumnos_with_row_nums
+WHERE row_id < 6; 
