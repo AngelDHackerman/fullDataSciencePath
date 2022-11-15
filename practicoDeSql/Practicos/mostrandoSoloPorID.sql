508c2aa7633b4a8bf64333906db26819d7b8fc38
angelDariaux <angeldariaux@gmail.com>
2022-11-14 21:57:02 -0600

    mostrando los que NO estan en el ID del subquery

--- practicoDeSql/Practicos/mostrandoSoloPorID.sql
+++ practicoDeSql/Practicos/mostrandoSoloPorID.sql
@@ -16,3 +16,14 @@ WHERE id IN ( -- este subquery nos devuelve los ID generales que tiene el tutor_
 	FROM platzi.alumnos
 	WHERE tutor_id = 30
 )
+
+
+-- ! Aqui se muestran los que NO estan el los ID indicados 
+
+SELECT * 
+FROM platzi.alumnos 
+WHERE id NOT IN ( 
+	SELECT id
+	FROM platzi.alumnos
+	WHERE tutor_id = 30
+)
