f919270b387b39cb19e9d78619c5ad747468d840
angelDariaux <angeldariaux@gmail.com>
2022-11-15 21:51:04 -0600

    extrayendo year, meses y dias de la columna

--- practicoDeSql/fechas.sql
+++ practicoDeSql/fechas.sql
@@ -3,3 +3,14 @@
 
 SELECT EXTRACT(YEAR FROM fecha_incorporacion) AS year_incorporacion 
 FROM platzi.alumnos;
+
+-- Exactamente lo mismo pero diferente forma: 
+
+SELECT DATE_PART('YEAR', fecha_incorporacion) AS year_incorporacion
+FROM platzi.alumnos; 
+
+
+SELECT DATE_PART('YEAR', fecha_incorporacion) AS year_incorporacion,
+		DATE_PART('MONTH', fecha_incorporacion) AS mes_incorporacion,
+		DATE_PART('DAY', fecha_incorporacion) AS dia_incorporacion
+FROM platzi.alumnos; 
