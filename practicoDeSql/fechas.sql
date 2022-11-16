5d92277f475ff7576d421bff38d2457a22774e62
angelDariaux <angeldariaux@gmail.com>
2022-11-15 21:53:01 -0600

    extrayendo horas minutos y segundos

--- practicoDeSql/fechas.sql
+++ practicoDeSql/fechas.sql
@@ -10,7 +10,19 @@ SELECT DATE_PART('YEAR', fecha_incorporacion) AS year_incorporacion
 FROM platzi.alumnos; 
 
 
+
+-- año, mes y dias: 
+
 SELECT DATE_PART('YEAR', fecha_incorporacion) AS year_incorporacion,
 		DATE_PART('MONTH', fecha_incorporacion) AS mes_incorporacion,
 		DATE_PART('DAY', fecha_incorporacion) AS dia_incorporacion
+FROM platzi.alumnos; 
+
+
+
+-- Horas, minutos y segundos: 
+
+SELECT DATE_PART('HOUR', fecha_incorporacion) AS hora_incorporacion,
+		DATE_PART('MINUTE', fecha_incorporacion) AS minuto_incorporacion,
+		DATE_PART('SECOND', fecha_incorporacion) AS segundo_incorporacion
 FROM platzi.alumnos; 
