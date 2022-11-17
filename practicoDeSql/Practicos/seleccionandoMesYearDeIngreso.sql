83d633274366ca8f7ae5add4e651704cbcffda22
angelDariaux <angeldariaux@gmail.com>
2022-11-16 21:32:17 -0600

    otra opcion pero usando date_part

--- practicoDeSql/Practicos/seleccionandoMesYearDeIngreso.sql
+++ practicoDeSql/Practicos/seleccionandoMesYearDeIngreso.sql
@@ -2,3 +2,16 @@ SELECT *
 FROM platzi.alumnos 
 WHERE (EXTRACT(YEAR FROM fecha_incorporacion)) = 2018 
 AND (EXTRACT(MONTH FROM fecha_incorporacion)) = 05;
+
+
+-- otra opcion: 
+
+SELECT * 
+FROM ( 
+	SELECT *, 
+		DATE_PART('YEAR', fecha_incorporacion) AS anio_incorporacion,
+		DATE_PART('MONTH', fecha_incorporacion) AS mes_incorporacion
+	FROM platzi.alumnos
+) AS alumnos_con_anio
+WHERE anio_incorporacion = 2018
+AND mes_incorporacion = 5;
