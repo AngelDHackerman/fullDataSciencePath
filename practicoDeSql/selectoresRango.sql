08f23c4e1a6505919b13d7e5c0a1ab93763b3c97
angelDariaux <angeldariaux@gmail.com>
2022-11-19 20:35:51 -0600

    usando between

--- practicoDeSql/selectoresRango.sql
+++ practicoDeSql/selectoresRango.sql
@@ -3,3 +3,10 @@
 
 SELECT * FROM platzi.alumnos 
 WHERE tutor_id IN (1, 2, 3, 4, 5)
+
+
+-- ? Tambien podemos buscar por rango usando BETWEEN 
+
+SELECT * FROM platzi.alumnos 
+WHERE tutor_id BETWEEN 1 AND 20
+ORDER BY tutor_id 
