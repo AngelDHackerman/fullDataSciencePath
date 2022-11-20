07bf30833d3d77ecddc6c3c88870deb28eb9268b
angelDariaux <angeldariaux@gmail.com>
2022-11-19 21:40:34 -0600

    agrupando filas y encontrando el valor minimo

--- practicoDeSql/Practicos/minimoMaximo.sql
+++ practicoDeSql/Practicos/minimoMaximo.sql
@@ -6,3 +6,14 @@ SELECT carrera_id, MAX(fecha_incorporacion)
 FROM platzi.alumnos 
 GROUP BY carrera_id
 ORDER BY carrera_id DESC;
+
+
+
+-- ? Ordenando por el minimo: 
+-- Devuelve el primer alumno en la lista (ordenado de manera alfabetica)
+-- segun la lista del tutor_id
+
+SELECT tutor_id, MIN(nombre)
+FROM platzi.alumnos
+GROUP BY tutor_id
+ORDER BY tutor_id;
