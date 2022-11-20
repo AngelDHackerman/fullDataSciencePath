0a5f2c58705b40fddcb96a855a8dc4640ce5fe24
angelDariaux <angeldariaux@gmail.com>
2022-11-19 20:41:43 -0600

    numrange

--- practicoDeSql/selectoresRango.sql
+++ practicoDeSql/selectoresRango.sql
@@ -17,3 +17,7 @@ ORDER BY tutor_id
 SELECT int4range (1, 20) @> 3;  -- Esto nos devuelve un boolean, dependiendo si el 3 esta o no en el rango especificado
 
 
+-- ? Numrange 
+-- Devuelve un boolean si el rango de la derecha se "solapa" en algun punto con el de la izquierda. 
+
+SELECT numrange(11.1, 22.2) && numrange (20.0, 30.0);
