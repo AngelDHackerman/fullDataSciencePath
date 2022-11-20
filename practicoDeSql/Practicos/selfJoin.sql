ec2369ee02315c64c86f37c8f74eec60bee314b7
angelDariaux <angeldariaux@gmail.com>
2022-11-19 22:03:29 -0600

    self join, mostrando los tutores con mas alumnos

--- practicoDeSql/Practicos/selfJoin.sql
+++ practicoDeSql/Practicos/selfJoin.sql
@@ -1,6 +1,5 @@
 
 -- ? Este script muestra primero el nombre del alumno y luego su tutor asignado.
-
 -- Notese este inner join usa la misma tabla para hacer la operacion.
 
 
@@ -10,3 +9,25 @@ SELECT
 FROM platzi.alumnos AS a
 	INNER JOIN platzi.alumnos AS t ON a.tutor_id = t.id;
 
+
+
+-- ? Este nos muestra que tutor tiene mas alumnos
+
+SELECT 
+    CONCAT(t.nombre, ' ', t.apellido) AS tutor,
+		COUNT(*) AS alumnos_por_tutor  -- Aqui se hace la cuenta de alumnos por tutor
+FROM platzi.alumnos AS a
+	INNER JOIN platzi.alumnos AS t 
+	ON a.tutor_id = t.id
+GROUP BY tutor
+ORDER BY alumnos_por_tutor DESC;
+
+
+SELECT CONCAT(t.nombre, ' ', t.apellido) AS tutor,
+		COUNT(*) AS alumnos_por_tutor
+FROM platzi.alumnos AS a
+	INNER JOIN platzi.alumnos AS t 
+	ON a.tutor_id = t.id
+GROUP BY tutor
+ORDER BY alumnos_por_tutor DESC
+LIMIT 10;
