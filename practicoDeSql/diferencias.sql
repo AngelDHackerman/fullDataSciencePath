45b7b607836b57190bde21de8fe5d13dcebbe8dd
angelDariaux <angeldariaux@gmail.com>
2022-11-20 15:29:37 -0600

    full outer joins

--- practicoDeSql/diferencias.sql
+++ practicoDeSql/diferencias.sql
@@ -80,3 +80,19 @@ FROM 	platzi.alumnos AS a
 	INNER JOIN platzi.carreras AS c
 	ON a.carrera_id = c.id
 ORDER BY C.id DESC;
+
+
+
+      -- ! FULL OUTER JOIN
+
+SELECT  a.nombre,
+		a.apellido,
+		a.carrera_id,
+		c.id,
+		c.carrera
+FROM 	platzi.alumnos AS a
+	FULL OUTER JOIN platzi.carreras AS c
+	ON a.carrera_id = c.id
+WHERE a.id IS NULL 
+	OR c.id IS NULL
+ORDER BY a.carrera_id DESC, c.id DESC;
