2cd8e1230e51c4ec822db1138ca7e266c626006b
angelDariaux <angeldariaux@gmail.com>
2022-11-22 11:52:22 -0600

    last_value function

--- practicoDeSql/windowFunctions.sql
+++ practicoDeSql/windowFunctions.sql
@@ -1,7 +1,18 @@
 
--- Mostrando una columna con el numero del "row" y ordenad por la fecha
+-- ? Mostrando una columna con el numero del "row" y ordenad por la fecha
 
 SELECT ROW_NUMBER() OVER(ORDER BY fecha_incorporacion) AS row_id, *
 FROM platzi.alumnos;
 
 
+-- ? La columna "row_id" aqui toma el valor de la primer colegiatura que la query encuentra:
+
+SELECT FIRST_VALUE(colegiatura) OVER() AS row_id, *
+FROM platzi.alumnos;
+
+
+-- ? Muestra la "ultima" o la colegiatura mas baja que tiene la "carrera_id" en sus datos
+
+SELECT  LAST_VALUE(colegiatura) 
+        OVER(PARTITION BY carrera_id) AS ultima_colegiatura, *
+FROM platzi.alumnos;
