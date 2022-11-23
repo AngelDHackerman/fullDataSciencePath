b6eb08f748d56b018f91de3ed229bb31ba060d0f
angelDariaux <angeldariaux@gmail.com>
2022-11-22 19:59:23 -0600

    agregados algunos comentarios

--- practicoDeSql/windowFunctions.sql
+++ practicoDeSql/windowFunctions.sql
@@ -5,20 +5,20 @@ SELECT ROW_NUMBER() OVER(ORDER BY fecha_incorporacion) AS row_id, *
 FROM platzi.alumnos;
 
 
--- ? La columna "row_id" aqui toma el valor de la primer colegiatura que la query encuentra:
+-- ? Muestra el primer valor de la colegiatura de "carrera_id" en la columna de "row" primer_colegiatura
 
-SELECT FIRST_VALUE(colegiatura) OVER() AS row_id, *
+SELECT FIRST_VALUE(colegiatura) OVER() AS primer_colegiatura, *
 FROM platzi.alumnos;
 
 
--- ? Muestra la "ultima" o la colegiatura mas baja que tiene la "carrera_id" en sus datos
+-- ? Muestra la "ultima" o la colegiatura mas baja que tiene la "carrera_id", en la columna de "ultima_colegiatura"
 
 SELECT  LAST_VALUE(colegiatura) 
         OVER(PARTITION BY carrera_id) AS ultima_colegiatura, *
 FROM platzi.alumnos;
 
 
--- ? Muestra (en este caso) la tercera colegiatura que tiene la "carrera_id" en la tabla
+-- ? Muestra (en este caso) la tercera colegiatura que tiene la "carrera_id" en la tabla, en la columna de "tercera colegiatura"
 
 SELECT NTH_VALUE(colegiatura, 3) OVER(PARTITION BY carrera_id) AS tercera_colegiatura, *
 FROM platzi.alumnos;
