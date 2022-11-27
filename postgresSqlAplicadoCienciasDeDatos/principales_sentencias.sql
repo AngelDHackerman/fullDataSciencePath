8a472cfc6be2b9df13d730b96332cd7c14e81a3e
angelDariaux <angeldariaux@gmail.com>
2022-11-27 07:50:45 -0600

    repasando sentencias del curso pasado

--- postgresSqlAplicadoCienciasDeDatos/principales_sentencias.sql
+++ postgresSqlAplicadoCienciasDeDatos/principales_sentencias.sql
@@ -1,15 +1,6 @@
 
 -- repaso de las sentencias conocidas. 
 
-SELECT MAX(ultima_actualizacion) AS fecha_ultima_actualizacion,
-			clasificacion,
-			COUNT(*) AS cantidad_peliculas
-FROM peliculas
-WHERE duracion_renta > 3
-GROUP BY clasificacion, ultima_actualizacion
-ORDER BY fecha_ultima_actualizacion;
-
-
 
 SELECT MAX(ultima_actualizacion) AS fecha_ultima_actualizacion, 
 		clasificacion,
