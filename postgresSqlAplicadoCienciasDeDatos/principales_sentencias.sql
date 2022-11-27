946915eabd360a4256e796fcda25364f3049e5bb
angelDariaux <angeldariaux@gmail.com>
2022-11-27 07:38:26 -0600

    repasando sentencias del curso pasado

--- postgresSqlAplicadoCienciasDeDatos/principales_sentencias.sql
+++ postgresSqlAplicadoCienciasDeDatos/principales_sentencias.sql
@@ -1,3 +1,6 @@
+
+-- repaso de las sentencias conocidas. 
+
 SELECT MAX(ultima_actualizacion) AS fecha_ultima_actualizacion,
 			clasificacion,
 			COUNT(*) AS cantidad_peliculas
@@ -6,3 +9,12 @@ WHERE duracion_renta > 3
 GROUP BY clasificacion, ultima_actualizacion
 ORDER BY fecha_ultima_actualizacion;
 
+
+
+SELECT MAX(ultima_actualizacion) AS fecha_ultima_actualizacion, 
+		clasificacion,
+		COUNT(*) AS cantidad_peliculas
+FROM peliculas
+WHERE duracion_renta > 3
+GROUP BY clasificacion, ultima_actualizacion  -- GROUP BY, creara conjuntos con las peliculas que tengan los mismos valores de clasificacion y ultima_actulizacion
+ORDER BY fecha_ultima_actualizacion
