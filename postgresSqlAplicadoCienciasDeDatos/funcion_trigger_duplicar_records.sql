d90694748bfd1f6bfa6746980d351ced3b46378d
angelDariaux <angeldariaux@gmail.com>
2022-11-28 21:32:03 -0600

    creando el trigger de la funcion

--- postgresSqlAplicadoCienciasDeDatos/funcion_trigger_duplicar_records.sql
+++ postgresSqlAplicadoCienciasDeDatos/funcion_trigger_duplicar_records.sql
@@ -9,12 +9,18 @@ RETURNS TRIGGER
 LANGUAGE plpgsql
 AS $$
 BEGIN
-	INSERT INTO aaab(bbba, ccca) -- insertar datos en las columnas bbba y ccca de la tabla aaab
-	VALUES (NEW.bbb, NEW.ccc); -- "New" es un valor en todas las funciones, cuando algo nuevo sea ingresado en bbb o en ccc de la "aaa", seran tambien introducidos en las columnas antes seleccionadas
-	RETURN NEW; -- muestre en el data output lo que fue ingresado.
+	INSERT INTO aaab(bbba, ccca) -- * insertar datos en las columnas bbba y ccca de la tabla aaab
+	VALUES (NEW.bbb, NEW.ccc); -- * "New" es un valor en todas las funciones, cuando algo nuevo sea ingresado en bbb o en ccc de la "aaa", seran tambien introducidos en las columnas antes seleccionadas
+	RETURN NEW; -- * muestre en el data output lo que fue ingresado.
 END
 $$;
 
 
--- ! Creando el trigger en si: 
+    -- ! Creando el trigger en si: 
+-- Se traduce asi: crea el triger "aaa_changes" antes de insertar datos en la tabla "aaa" y por cada fila, ejecuta el prodecure "duplicate_records()"
 
+CREATE TRIGGER aaa_changes  -- * creando el triger de la funcion antes creada
+	BEFORE INSERT -- * antes de insertar los datos
+	ON aaa -- * se especifica que tabla activara el trigger 
+	FOR EACH ROW 
+	EXECUTE PROCEDURE duplicate_records();
