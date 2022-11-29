d1f58b166b5737efb44d8ba088bb7fd30f62fac5
angelDariaux <angeldariaux@gmail.com>
2022-11-28 20:44:54 -0600

    creando una tabla usando procedures

---
+++ postgresSqlAplicadoCienciasDeDatos/stored_procedures.sql
@@ -0,0 +1,9 @@
+
+    -- ? Creando una tabla con un procedure. 
+
+CREATE OR REPLACE PROCEDURE test_dropcreate_procedure() -- ? Asi es como se crean los procedures
+LANGUAGE SQL  -- se debe indicar el lenguaje a usar
+AS $$  -- ? todo lo que este dentro de los \$\$ sera el en lengujae plpgSql, para el prodecedure
+	DROP TABLE IF EXISTS aaa;
+	CREATE TABLE aaa (bbb char(5) CONSTRAINT firstkey PRIMARY KEY); -- crea la tabla con el nombre "aaa" tiene un campo de nombre "bbb" con characteres de longitud 5, y una llave primaria
+$$
