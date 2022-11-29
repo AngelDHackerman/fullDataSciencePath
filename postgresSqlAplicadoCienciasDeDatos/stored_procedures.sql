127816331f9a2f75be4495f0b7200fb08e925b02
angelDariaux <angeldariaux@gmail.com>
2022-11-28 20:53:58 -0600

    invocando prodecures

--- postgresSqlAplicadoCienciasDeDatos/stored_procedures.sql
+++ postgresSqlAplicadoCienciasDeDatos/stored_procedures.sql
@@ -7,3 +7,7 @@ AS $$  -- ? todo lo que este dentro de los \$\$ sera el en lengujae plpgSql, par
 	DROP TABLE IF EXISTS aaa;
 	CREATE TABLE aaa (bbb char(5) CONSTRAINT firstkey PRIMARY KEY); -- crea la tabla con el nombre "aaa" tiene un campo de nombre "bbb" con characteres de longitud 5, y una llave primaria
 $$
+
+    -- ? Y de esta manera se invocan los procedures: 
+
+CALL test_dropcreate_procedure();
