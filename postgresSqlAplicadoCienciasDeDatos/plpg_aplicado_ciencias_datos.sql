9a588f36813e6948eae347287aceefeac5ddfc8d
angelDariaux <angeldariaux@gmail.com>
2022-11-29 21:58:19 -0600

    insertando los datos resultantes en la tabla trucada, y viendo los totales y promedios

--- postgresSqlAplicadoCienciasDeDatos/plpg_aplicado_ciencias_datos.sql
+++ postgresSqlAplicadoCienciasDeDatos/plpg_aplicado_ciencias_datos.sql
@@ -15,12 +15,27 @@ BEGIN
 	total_rated_r := COUNT(*) FROM peliculas WHERE clasificacion = 'R';
 	total_larger_than_100 := COUNT (*) FROM peliculas WHERE duracion > 100;
 	total_published_2006 := COUNT(*) FROM peliculas WHERE anio_publicacion = 2006;
-	
 	average_duracion := AVG(duracion) FROM peliculas;
 	average_rental_price := AVG (precio_renta) FROM peliculas;
 	
-	TRUNCATE TABLE peliculas_estadisticas -- Borra el contenido de la tabla, pero no la tabla tal cual
+	TRUNCATE TABLE peliculas_estadisticas; -- Borra el contenido de la tabla, pero no la tabla tal cual
 	
 	INSERT INTO peliculas_estadisticas (tipo_estadistica, total)  -- todo lo que generamos con las variables anteriores sera introducido aqui en esta tabla, en sus 2 columnas
+	VALUES 
+	('Peliculas con clasificacion R', total_rated_r),  -- el primer valor se va a columna 'tipo_estadistica' y el segundo valor a 'total'
+	('Peliculas de mas de 100 minutos', total_larger_than_100),
+	('Peliculas publicadas en 2006', total_published_2006),
+	('Promedio de duracion en minutos', average_duration),
+	('Precio promedio de renta', average_rental_price)
 END
 $$
+
+
+
+
+
+
+
+
+
+
