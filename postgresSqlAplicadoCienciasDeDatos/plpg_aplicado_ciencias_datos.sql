7b51ae2b927ca16669b4127f708d1a1a089d7731
angelDariaux <angeldariaux@gmail.com>
2022-11-29 21:54:02 -0600

    usando el trucate para borrar los datos de la tabla pero no la tabla

--- postgresSqlAplicadoCienciasDeDatos/plpg_aplicado_ciencias_datos.sql
+++ postgresSqlAplicadoCienciasDeDatos/plpg_aplicado_ciencias_datos.sql
@@ -1,5 +1,5 @@
 
--- ? Usando variables y sacando los promedios de duraction, precio entre otros
+-- ? Usando variables y sacando los promedios y totales de nuestra tabla peliculas
 
 CREATE OR REPLACE FUNCTION movies_stats()
 RETURNS VOID
@@ -12,8 +12,15 @@ DECLARE -- asi es como se declaran las variables sen plpgsql, para asignar valor
 	average_duration REAL := 0.0;
 	average_rental_price REAL := 0.0;
 BEGIN
-	total_rated_r := COUNT(*) FROM peliculas WHERE clasificacion = 'R'
-
+	total_rated_r := COUNT(*) FROM peliculas WHERE clasificacion = 'R';
+	total_larger_than_100 := COUNT (*) FROM peliculas WHERE duracion > 100;
+	total_published_2006 := COUNT(*) FROM peliculas WHERE anio_publicacion = 2006;
+	
+	average_duracion := AVG(duracion) FROM peliculas;
+	average_rental_price := AVG (precio_renta) FROM peliculas;
+	
+	TRUNCATE TABLE peliculas_estadisticas -- Borra el contenido de la tabla, pero no la tabla tal cual
+	
+	INSERT INTO peliculas_estadisticas (tipo_estadistica, total)  -- todo lo que generamos con las variables anteriores sera introducido aqui en esta tabla, en sus 2 columnas
 END
 $$
-
