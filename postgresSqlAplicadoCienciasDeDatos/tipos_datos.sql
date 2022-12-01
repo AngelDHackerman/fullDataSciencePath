e6eca8f40fd731699e3f432b84ea00679775d210
angelDariaux <angeldariaux@gmail.com>
2022-11-30 21:26:51 -0600

    usando tipos de datos

---
+++ postgresSqlAplicadoCienciasDeDatos/tipos_datos.sql
@@ -0,0 +1,21 @@
+
+-- ? Asi se crean los tipos de datos
+
+CREATE TYPE humor AS ENUM ('triste', 'normal', 'feliz'); -- ENUM es como un array.
+
+
+-- ? creando una tabla con el nuevo tipo de dato
+
+CREATE TABLE persona_prueba(
+	nombre text,  -- * tipo de dato es "text"
+	humor_actual humor  -- * tipo de dato es "humor"
+)
+
+
+-- todo: Este query no funciona porque en el valor enum, 'molesto' no esta incluido.
+
+INSERT INTO persona_prueba VALUES ('pablo', 'molesto');
+
+-- todo: Este si funciona porque el valor en ENUM si esta incluido:
+
+INSERT INTO persona_prueba VALUES ('pablo', 'feliz');
