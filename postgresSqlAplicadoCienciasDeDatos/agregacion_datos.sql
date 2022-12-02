4ecf83f03265f29e186e904c28d059ea1c18449d
angelDariaux <angeldariaux@gmail.com>
2022-12-01 20:28:34 -0600

    funcion suma

--- postgresSqlAplicadoCienciasDeDatos/agregacion_datos.sql
+++ postgresSqlAplicadoCienciasDeDatos/agregacion_datos.sql
@@ -12,9 +12,17 @@ FROM peliculas
 GROUP BY titulo, precio_renta
 ORDER BY precio_renta DESC;
 
+
 -- ? Sacando el precio minimo de renta segun el titulo: 
 
 SELECT titulo, MIN(precio_renta)
 FROM peliculas
 GROUP BY titulo, precio_renta
 ORDER BY precio_renta;
+
+
+-- ? Precio total de rentar todas las peliculas: 
+
+SELECT SUM(precio_renta)
+FROM peliculas;
+
