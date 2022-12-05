2bdc36ce0c70d36149f41d6554f65125ebec13e5
angelDariaux <angeldariaux@gmail.com>
2022-12-04 18:25:19 -0600

    insertando valores json

--- postgresSqlAplicadoCienciasDeDatos/objetos_sql.sql
+++ postgresSqlAplicadoCienciasDeDatos/objetos_sql.sql
@@ -1,7 +1,22 @@
 
 -- ? SQL con objetos json 
 
-CREATE TABLE (  -- asi se crean las tablas
+CREATE TABLE ordenes (  -- asi se crean las tablas
 	ID serial NOT NULL PRIMARY KEY,  -- es una llave primaria que no puede tener valores nulos
 	info json NOT NULL -- Indicamos que sera para formatos json
 )
+
+
+-- ? Insertando valores en la tabla
+
+INSERT INTO ordenes (info) -- * los datos seran introducidos en info y el id se genera automaticamente.
+VALUES  -- * abajo se muestran como se introducen los datos.
+	(
+		'{"cliente": "David Sanchez", "items": {"producto":"Biberon", "cantidad": "24"}}'
+	),
+	(
+		'{"cliente": "Angel Hernandez", "items": {"producto":"Carrito juguete", "cantidad": "1"}}'
+	),
+	(
+		'{"cliente": "Israel vasquez", "items": {"producto":"tren mini", "cantidad": "3"}}'
+	);
