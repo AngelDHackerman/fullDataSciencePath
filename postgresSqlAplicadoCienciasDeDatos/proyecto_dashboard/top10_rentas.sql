edc4d9d4ed84fb2b334f2d15158f81a3d866d2c7
angelDariaux <angeldariaux@gmail.com>
2022-12-05 21:55:28 -0600

    top 10 pero con un indice en la tabla que indica el lugar en el top 10 de cada pelicula

--- postgresSqlAplicadoCienciasDeDatos/proyecto_dashboard/top10_rentas.sql
+++ postgresSqlAplicadoCienciasDeDatos/proyecto_dashboard/top10_rentas.sql
@@ -14,3 +14,19 @@ ORDER BY numero_rentas DESC
 LIMIT 10;
 
 
+
+-- ? Agregando un indice a la tabla mostrada con el query anterior
+
+SELECT 
+	peliculas.pelicula_id AS id,
+	peliculas.titulo,
+	COUNT(*) AS numero_rentas,
+	ROW_NUMBER () OVER (  -- * ROW_NUMBER, es la window function que contara las filas, y OVER, le indica a 'row_number', sobre que hacer la cuenta, en este caso el total mostrado de la tabla
+		ORDER BY COUNT(*) DESC
+	) AS lugar
+FROM rentas	
+	INNER JOIN inventarios ON rentas.inventario_id = inventarios.inventario_id
+	INNER JOIN peliculas ON inventarios.pelicula_id = peliculas.pelicula_id
+GROUP BY peliculas.pelicula_id
+ORDER BY numero_rentas DESC
+LIMIT 10;
