8e646a010197e514b8bf9a915b3fdbfe4acc13eb
angelDariaux <angeldariaux@gmail.com>
2022-12-07 19:57:33 -0600

    sql para datos geograficos

--- postgresSqlAplicadoCienciasDeDatos/proyecto_dashboard/datos_geograficos.sql
+++ postgresSqlAplicadoCienciasDeDatos/proyecto_dashboard/datos_geograficos.sql
@@ -8,4 +8,7 @@ FROM ciudades
 	INNER JOIN direcciones ON ciudades.ciudad_id = direcciones.ciudad_id  -- * Enlazamos la tabla de 'direcciones' con la de 'ciudades' por medio de 'ciudad_id'
 	INNER JOIN tiendas ON tiendas.direccion_id = direcciones.direccion_id  -- * Enlazamos 'tiendas' con 'direcciones' por medio de 'direccion_id'
 	INNER JOIN inventarios ON inventarios.tienda_id = tiendas.tienda_id
+	INNER JOIN rentas ON inventarios.inventario_id = rentas.inventario_id
+GROUP BY ciudades.ciudad_id
+ORDER BY rentas_por_ciudad DESC;
 	
