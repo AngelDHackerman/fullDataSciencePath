7deabb590626c63d54de25b3a3b27ee68915b9de
angelDariaux <angeldariaux@gmail.com>
2022-12-07 19:52:55 -0600

    creando los inner joins para enlazar las tablas

---
+++ postgresSqlAplicadoCienciasDeDatos/proyecto_dashboard/datos_geograficos.sql
@@ -0,0 +1,11 @@
+
+-- ? Este script ayuda para hacer graficas de tipo mapa
+
+SELECT ciudades.ciudad_id,
+		ciudades.ciudad,
+		COUNT (*) AS rentas_por_ciudad  -- * esto va hacer la cuenta, segun como lo agrupemos
+FROM ciudades
+	INNER JOIN direcciones ON ciudades.ciudad_id = direcciones.ciudad_id  -- * Enlazamos la tabla de 'direcciones' con la de 'ciudades' por medio de 'ciudad_id'
+	INNER JOIN tiendas ON tiendas.direccion_id = direcciones.direccion_id  -- * Enlazamos 'tiendas' con 'direcciones' por medio de 'direccion_id'
+	INNER JOIN inventarios ON inventarios.tienda_id = tiendas.tienda_id
+	
