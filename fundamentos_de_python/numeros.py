fa96c31b693003dc4a477c2558a6f868b8d73dd4
angel hernandez <angel.hernandez_santiago@telus.com>
2022-12-12 09:16:45 -0600

    numeros y tipos de numeros

---
+++ fundamentos_de_python/numeros.py
@@ -0,0 +1,43 @@
+
+lives = 3
+print(type(lives))
+age = 12 
+budget = 300
+
+
+temperature = 12.12 
+print(type(temperature)) 
+
+
+lives = 2 
+print(lives)
+lives = 1 
+print(lives)
+
+
+lives = 12  + 15 
+print(lives)
+
+lives = lives - 1 
+print(lives)
+
+    # * Sumando y restando de forma simplificada 
+
+lives -= 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
+
+
+
+    # * Numeros con formato grande 
+
+numero = 46000000000000000000000000000000.1
+print(numero)
+
+numero_b = 0.0000000000000000000000000000000000000001
+print(numero_b)
