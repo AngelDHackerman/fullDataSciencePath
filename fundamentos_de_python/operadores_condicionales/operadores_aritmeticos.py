524c79893fdf04a6e47b5b314ad831967351e40b
angel hernandez <angel.hernandez_santiago@telus.com>
2022-12-12 13:59:34 -0600

    operadores aritmeticos

---
+++ fundamentos_de_python/operadores_condicionales/operadores_aritmeticos.py
@@ -0,0 +1,16 @@
+
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2) # 5
+print(10 % 2) # ? 0, este es el percentil
+print(10 / 3)
+print(10 % 3) # 1
+print(10 // 3)  # ?, el simbolo // da el resultado sin mostrar los decimales
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+
+print(f'Hola Mundo')
+print('Hola ' * 3)
+print()
