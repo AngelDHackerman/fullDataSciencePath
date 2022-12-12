b498f71ff8df56f3f8f3d47694a695fa979a9e59
angelDariaux <angeldariaux@gmail.com>
2022-12-11 20:53:18 -0600

    basico de python

---
+++ fundamentos_de_python/variables.py
@@ -0,0 +1,21 @@
+
+# * Esto es una variable 
+
+my_name = "Nicolas"
+print(my_name)
+
+
+# * Esto es una variable con un numero
+
+my_age = 27 
+print(my_age)
+print(type(my_age))
+
+
+
+print('\n')
+# * Input 
+
+my_name = input('Cual es tu nombre? ')
+print(f'usando input {my_name}')
+
