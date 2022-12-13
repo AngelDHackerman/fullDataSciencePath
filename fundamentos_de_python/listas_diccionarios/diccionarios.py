f16dec4990c822273d172955cad9261b4517a070
angel hernandez <angel.hernandez_santiago@telus.com>
2022-12-13 15:54:20 -0600

    diccionarios

--- fundamentos_de_python/listas_diccionarios/diccionarios.py
+++ fundamentos_de_python/listas_diccionarios/diccionarios.py
@@ -10,6 +10,11 @@ my_dict = {
 }
 
 print(my_dict)
-print(len(my_dict))
+print('len =>', len(my_dict)) # 4 valores
 
-print(my_dict['age'])
+print(my_dict['age'])  # * Si buscamos segun la clave, luego nos dara el valor
+print(my_dict['last_name'])
+print(my_dict.get('age'))  # * funciona igual si usamos el metodo "get" para buscar el valor segun su clave.
+
+print('avion' in my_dict)  # True, porque verificia si la clave "avion" esta en el diccionario.
+print('este_no' in my_dict)  # False.
