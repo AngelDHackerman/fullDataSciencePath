626295529156baf053c864001af5412a487698ba
angel hernandez <angel.hernandez_santiago@telus.com>
2022-12-13 15:50:43 -0600

    metodos sobre las tuplas

---
+++ fundamentos_de_python/listas_diccionarios/diccionarios.py
@@ -0,0 +1,15 @@
+
+my_dict = {}
+print(type(my_dict))
+
+my_dict = { 
+  'avion': 'bla bla bla',
+  'name': 'Angel',
+  'last_name': 'Hernandez',
+  'age': 27
+}
+
+print(my_dict)
+print(len(my_dict))
+
+print(my_dict['age'])
