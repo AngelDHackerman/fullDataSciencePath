e7f4d3c4ff62e499659bc04033154338e6b47495
angelDariaux <angeldariaux@gmail.com>
2022-12-12 20:38:59 -0600

    metodos sobre los strings

--- fundamentos_de_python/listas_diccionarios/string_recargado.py
+++ fundamentos_de_python/listas_diccionarios/string_recargado.py
@@ -1,9 +1,9 @@
 
-text = 'Ella sabe programar en Python'
+text = 'Ella sabe programar en Python.'
 print('Javascript' in text)  # ? In, verifica si el valor de la izquierda se encuentra en la variable de la derecha
 print('Python' in text)  # True
 
-      # ? Metodo In:
+      # ? Metodo In():
 
 if 'Python' in text:
   print('Elegiste bien!!!')
@@ -13,7 +13,50 @@ else:
 
 
 print('\n')
-      # ? len, largo del texto
+      # ? Metodo len(), largo del texto
 
 size = len(text)
-print(size)
+print(f'Hay {size} caracteres en ese texto')
+
+
+
+      # ? Metodos upper(), lower().
+
+print(text, text.upper())  # transforma todo a mayusculas
+print(text, text.lower())  # transforma todo a minusculas
+
+
+
+print('\n')
+      # ? Metodo count()
+
+print(text.count('a'))  # cuanta las "a" en el texto
+
+
+
+print('\n')
+      # ? Metodo swapcase()
+
+print(text.swapcase())  # las minusculas pasan a mayusculas, y las mayusculas a minusculas
+
+
+
+print('\n')
+      # ? Metodo startswith(), endswith(), replace()
+
+print(text.startswith('Ella'))  # verifica si el string comienza con la palabra pasada como valor -> devuelve un boolean
+print(text.endswith('Rust')) # False
+print(text.replace('Python', 'Go'))  # ? Este es un metodo inmutable, no cambia el string original
+print(text)
+
+
+
+print('\n')
+      # ? Metodos capitalize(), title(), isdigit()
+
+text_2 = 'este es un titulo'
+print(text_2)
+print(text_2.capitalize())  # Inicial en mayuscula
+print(text_2.title())  # Cada inicial se devuelve en mayuscula
+print(text_2.isdigit())  # Ve si el valor es un digito -> regresa booleans
+print('398'.isdigit())  # * apesar de que es un "string", se reconoce como un numero (integer)
