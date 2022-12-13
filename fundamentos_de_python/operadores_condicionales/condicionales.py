814e4320cbe54c6200e74f648b0523e33423fe05
angelDariaux <angeldariaux@gmail.com>
2022-12-12 19:44:59 -0600

    condicionales if else

---
+++ fundamentos_de_python/operadores_condicionales/condicionales.py
@@ -0,0 +1,25 @@
+
+if True: 
+  print('Deberia ejecutarse')
+
+if False:
+  print('Nunca se ejecuta')
+
+pet = input('Cual es tu mascota favorita? ')
+
+if pet == 'perro':
+  print('Genial tienes buen gusto')
+
+if pet == 'gato':
+  print('Espero que tengas suerte')
+
+
+
+print('\n')
+stock = int(input('Digita el stock: '))
+
+if stock >= 100 and stock <= 1000:
+  print('El stock es correcto')
+else:
+  print('El stock es incorrecto')
+
