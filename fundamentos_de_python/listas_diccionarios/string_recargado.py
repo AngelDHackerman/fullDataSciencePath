a9db5600ab9fb45db03de4a34b3f07b860c2ebb3
angelDariaux <angeldariaux@gmail.com>
2022-12-12 20:26:46 -0600

    piedra papel o tijera

---
+++ fundamentos_de_python/listas_diccionarios/string_recargado.py
@@ -0,0 +1,19 @@
+
+text = 'Ella sabe programar en Python'
+print('Javascript' in text)  # ? In, verifica si el valor de la izquierda se encuentra en la variable de la derecha
+print('Python' in text)  # True
+
+      # ? Metodo In:
+
+if 'Python' in text:
+  print('Elegiste bien!!!')
+else:
+  print('Tambien elegiste bien')
+
+
+
+print('\n')
+      # ? len, largo del texto
+
+size = len(text)
+print(size)
