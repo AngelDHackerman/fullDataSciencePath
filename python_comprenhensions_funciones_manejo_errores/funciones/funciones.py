52a3442410aa6d45f1f4d7d87f0f07752f147ffd
angel hernandez <angel.hernandez_santiago@telus.com>
2022-12-15 08:15:14 -0600

    iniciando con las funciones

---
+++ python_comprenhensions_funciones_manejo_errores/funciones/funciones.py
@@ -0,0 +1,18 @@
+
+def my_print(text):  # la palabra clave "def" declara las funciones
+  print(text * 2)
+
+my_print('Este es my texto ')
+my_print('Hola ')
+
+
+
+a = 10
+b = 90
+
+c = a + b
+
+def suma(a, b):
+  print(a + b)
+
+suma(7, 2)
