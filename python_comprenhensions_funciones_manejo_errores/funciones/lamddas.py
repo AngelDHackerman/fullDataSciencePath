de795f2d09e9dc51d99c2520dc17b92410bb42c9
angelDariaux <angeldariaux@gmail.com>
2022-12-20 17:48:31 -0600

    usando funciones lambda

---
+++ python_comprenhensions_funciones_manejo_errores/funciones/lamddas.py
@@ -0,0 +1,7 @@
+
+# ? las funciones lambda son funciones anonimas con sintaxis simple
+
+increment_v2 = lambda x : x + 1  # el parametro "x" : "regrea" x + 1   # Y la funcion no tiene ningun nombre. Y se asigna a la variable increment_v2
+
+result = increment_v2(20)
+print(result)
