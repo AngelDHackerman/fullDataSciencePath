66ec3f0f0b57958308ed2883d5831983a701b9b6
angelDariaux <angeldariaux@gmail.com>
2022-12-20 17:54:29 -0600

    lambda con varios parametros

--- python_comprenhensions_funciones_manejo_errores/funciones/lamddas.py
+++ python_comprenhensions_funciones_manejo_errores/funciones/lamddas.py
@@ -5,3 +5,12 @@ increment_v2 = lambda x : x + 1  # el parametro "x" : "regrea" x + 1   # Y la fu
 
 result = increment_v2(20)
 print(result)
+
+
+
+# * Otra forma de hacer lambdas: 
+
+full_name = lambda name, last_name : f'Full name is {name.title()} and last name is: {last_name.title()}'  # .title: hace que cada primera letra se haga mayuscula
+
+text = full_name('Angel', 'Hernandez')
+print(text)
