6e79c9e06805aa6ba4faae3c46d1d072f2b38abe
angelDariaux <angeldariaux@gmail.com>
2022-12-20 21:44:30 -0600

    usando map para agregar un elemento mas en el objeto del array

---
+++ python_comprenhensions_funciones_manejo_errores/funciones/maps_y_diccionarios.py
@@ -0,0 +1,32 @@
+
+items = [
+  {
+    'product': 'camisa',
+    'price': 100
+  },
+  {
+  'product': 'pantalon',
+  'price': 300
+  },
+  {
+  'product': 'pantalon',
+  'price': 200
+  }
+]
+
+  # ? Aqui se transforma el array de diccionarios en un array de precios: 
+
+prices = list(map(lambda item: item['price'], items))  # * El PRIMER parametro itera sobre la clave/valor de item: price, el SEGUNDO indica cual es el array sobre el que hay que iterar
+print(prices) # [100, 300, 200]
+
+
+
+print('\n')
+  # ? Agregando un elemento extra al diccionario: 
+
+def add_taxes(item):
+  item['taxes'] = item['price'] * .19  # ? La clave sera 'taxes' y el valor es: 'price' * 0.19
+  return item
+
+new_items = list(map(add_taxes, items))  # ? "items", sera el parametro de la funcion "add_taxes" que a su vez esta funcion retorna la clave y el valor de los taxes 
+print(new_items)
