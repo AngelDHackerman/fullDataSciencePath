0aa387b55809a09d300c0c8aacf74a301b45b890
angel hernandez <angel.hernandez_santiago@telus.com>
2022-12-21 12:05:49 -0600

    usando el metodo .copy() de python para inmutabilidad

--- python_comprenhensions_funciones_manejo_errores/funciones/maps_y_diccionarios.py
+++ python_comprenhensions_funciones_manejo_errores/funciones/maps_y_diccionarios.py
@@ -23,10 +23,13 @@ print(prices) # [100, 300, 200]
 
 print('\n')
   # ? Agregando un elemento extra al diccionario: 
+  # todo: Esto modifica el array original. Esto es porque maps afecta a la referencia en memoria
 
 def add_taxes(item):
   item['taxes'] = item['price'] * .19  # ? La clave sera 'taxes' y el valor es: 'price' * 0.19
   return item
 
 new_items = list(map(add_taxes, items))  # ? "items", sera el parametro de la funcion "add_taxes" que a su vez esta funcion retorna la clave y el valor de los taxes 
-print(new_items)
+print(new_items) # Nuevo array con el elemento e items. 
+
+print(items) # este array y diccionario fueron mutados sin querer. 
