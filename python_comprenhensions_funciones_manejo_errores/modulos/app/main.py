d4556d81837e3ee054b8449c560c5a2b12f21f19
angelDariaux <angeldariaux@gmail.com>
2022-12-21 21:09:11 -0600

    creando mis propios modulos

---
+++ python_comprenhensions_funciones_manejo_errores/modulos/app/main.py
@@ -0,0 +1,6 @@
+
+import mods  # ? Este el archivo que esta al lado del main.py
+
+keys, values = mods.get_population()  # * asi se invoca el modulo y con .get_population() entramos a sus funciones. 
+
+print(keys, values)
---
+++ python_comprenhensions_funciones_manejo_errores/modulos/app/mods.py
@@ -0,0 +1,7 @@
+
+# todo: Los modulos en python son todos los archivos que terminen en ".py"
+
+def get_population():
+  keys = ['col', 'bol']
+  values = [300, 400]
+  return keys, values
