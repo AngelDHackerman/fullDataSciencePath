d685f3501560d0a3022009316b73f61eeae509ed
angel hernandez <angel.hernandez_santiago@telus.com>
2022-12-22 09:22:26 -0600

    agregando comentarios sobre importaciones

---
+++ python_comprenhensions_funciones_manejo_errores/modulos/paquetes/__init__.py
@@ -0,0 +1,7 @@
+
+# todo: Este archivo debe ser creado para importaciones solo en versiones inferiores python 3.3 
+# ? Este archivo se ejecuta cada que llamamos al archivo main.py
+
+print('Se inicio paquete')
+
+URL = 'platzi.com'
