3ade562f1c872de341a6064fb894d35a7ba00d33
angelDariaux <angeldariaux@gmail.com>
2022-12-25 19:39:26 -0600

    creando ambientes virtuales con anaconda

---
+++ entorno_trabajo_jupyter_notebook_anaconda/comandos_de_anaconda_.bash
@@ -0,0 +1,19 @@
+
+# ? Para crear un enviroment con anaconda debemos usar: 
+
+`conda create -n <nombre_env>`
+
+
+# ? Para listar y ver el enviroment en uso debemos usar: 
+
+`conda env list`
+
+
+# ? Activar el enviroment deseado: 
+
+`conda activate <nombre_env>`
+
+
+# ? Desactivar el enviroment: 
+
+`conda deactivate`
